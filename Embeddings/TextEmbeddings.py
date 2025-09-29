366c9706ba2004efcb3a04dbf99b77369b4dc8d3
Deepika Sridhar <126746947+DeepikaSridhar2112@users.noreply.github.com>
2025-09-29 13:27:46 +0530

    Update TextEmbeddings.py

--- Embeddings/TextEmbeddings.py
+++ Embeddings/TextEmbeddings.py
@@ -1,10 +1,18 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv(),override=True)
-
-from langchain.embeddings import OpenAIEmbeddings
-embeddings = OpenAIEmbeddings()
 import pandas as pd
+import numpy as np
+from langchain.embeddings import OpenAIEmbeddings
+
+load_dotenv(find_dotenv(),override=True)
+embeddings = OpenAIEmbeddings()
+
+def cosine_similarity(a, b):
+    a=a.replace('[','')
+    a=a.replace(']','')
+    a = list(map(float, a.split(',')))
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    
 df= pd.read_csv('embeddings_data.txt')
 df['embedding'] = df['Words'].apply(lambda x: embeddings.embed_query(x))
 df.to_csv('embeddings_with_words.txt')
@@ -14,13 +22,7 @@ print(df_with_embeddings['embedding'])
 my_input = "mango"
 text_embeddings = embeddings.embed_query(my_input)
 print(text_embeddings)
-import numpy as np
 
-def cosine_similarity(a, b):
-    a=a.replace('[','')
-    a=a.replace(']','')
-    a = list(map(float, a.split(',')))
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 df_with_embeddings['similarity_score'] = df_with_embeddings['embedding'].apply(lambda x: cosine_similarity(x, text_embeddings))
 print(df_with_embeddings['similarity_score'])
 df_with_embeddings.sort_values('similarity_score', ascending=False)
