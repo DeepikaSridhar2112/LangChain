2bf53c59a9ac32ea0b49cceba02b17fec7caa2bf
Deepika Sridhar <126746947+DeepikaSridhar2112@users.noreply.github.com>
2025-09-29 10:40:18 +0530

    Update ChatAppWithStreamLit.py

--- ChatModels/ChatAppWithStreamLit.py
+++ ChatModels/ChatAppWithStreamLit.py
@@ -1,1 +1,30 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+import streamlit as st
 
+def generate_response(query):
+    st.session_state.sessionMessages.append(HumanMessage(content=query))
+    chat = ChatOpenAI(
+        model = "gpt-turbo-3.5-turbo",
+        temperature = 0.3
+    )
+    response = chat(st.session_state.sessionMessages)
+    st.state.sessionMessages.append(AIMessage(content = response.context))
+    return response.content
+
+def get_inputs():
+    user_input= st.text_input("Prompt: ", key= "input")
+    return user_input
+
+st.header("chatmodel")
+st.set_page_config(page_title="chat-model-integration", page_icon=":rocket:")
+if "sessionMessages" not in st.session_state:
+    st.session_state.sessionMessages = [
+        SystemMessage(content="You are an assistant helping people")
+    ]
+    
+user_input = get_inputs()
+submit = st.button("Click me")
+
+if submit:
+    response = generate_response(user_input)
