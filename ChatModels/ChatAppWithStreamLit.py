4a29173418bb530c49386daae9fb3eee86e47736
Deepika Sridhar <126746947+DeepikaSridhar2112@users.noreply.github.com>
2025-09-29 11:36:14 +0530

    Update ChatAppWithStreamLit.py

--- ChatModels/ChatAppWithStreamLit.py
+++ ChatModels/ChatAppWithStreamLit.py
@@ -5,16 +5,17 @@ import streamlit as st
 def generate_response(query):
     st.session_state.sessionMessages.append(HumanMessage(content=query))
     chat = ChatOpenAI(
-        model = "gpt-3.5-turbo",
-        temperature = 0.3
+    model="gpt-3.5-turbo",  
+    temperature=0.7, 
     )
-    response = chat(st.session_state.sessionMessages)
-    st.state.sessionMessages.append(AIMessage(content = response.context))
+    response = chat([HumanMessage(content=query)])
+    st.state.sessionMessages.append(AIMessage(content = response.content))
     return response.content
 
 def get_inputs():
     user_input= st.text_input("Prompt: ", key= "input")
     return user_input
+
 
 st.header("chatmodel")
 st.set_page_config(page_title="chat-model-integration", page_icon=":rocket:")
@@ -24,7 +25,7 @@ if "sessionMessages" not in st.session_state:
     ]
     
 user_input = get_inputs()
-submit = st.button("Click me")
+submit = st.button('Click me')
 
 if submit:
     response = generate_response(user_input)
