6a8b27306fb9225862fbf39ad525d8a15b0be6d2
Deepika Sridhar <126746947+DeepikaSridhar2112@users.noreply.github.com>
2025-09-30 11:14:51 +0530

    Update MarketingApplication.py

--- Applications/MarketingApplication.py
+++ Applications/MarketingApplication.py
@@ -1,23 +1,76 @@
-def generate_response(age,task_type,user_query):
-    examples = [
-    {
-       "query": "What is a mobile" ,
-       "answer":"Its a magical device which lets us see people"
-    },
-    {
-        "query":"What happens when you get sick",
-        "answer":"I love to sleep and drink my tonic"
-    },
-    {
-        "query":"Do you like your mom or dad",
-        "answer":"I love my mom like sun and dad like moon"
-    },
-    {
-        "query":"What do you want to become",
-        "answer":"I want to become an icecream man"
-     }
-    ]
+from sqlite3 import TimeFromTicks
+from langchain.prompts import example_selector
+import streamlit as st
+from dotenv import load_dotenv,find_dotenv
+from langchain.llms import OpenAI
+from langchain import FewShotPromptTemplate
+from langchain.prompts.example_selector import LengthBasedExampleSelector
+from langchain.prompts import PromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+import streamlit as st
 
+load_dotenv(find_dotenv(),override=True)
+def generate_examples(age):
+    if(age == "kid"):
+            return [
+             {
+               "query": "What is a mobile" ,
+                "answer":"Its a magical device which lets us see people"
+             },
+             {
+                "query":"What happens when you get sick",
+               "answer":"I love to sleep and drink my tonic"
+             },
+             {
+                  "query":"Do you like your mom or dad",
+                   "answer":"I love my mom like sun and dad like moon"
+              },
+              {
+                    "query":"What do you want to become",
+                      "answer":"I want to become an icecream man"
+                 }
+             ]
+    if(age == "adult"):
+                    return [
+             {
+               "query": "What is a mobile" ,
+                "answer":"Its a device used for communication "
+             },
+             {
+                "query":"What happens when you get sick",
+               "answer":"I sleep well, recover and feel energetic"
+             },
+             {
+                  "query":"Do you like your mom or dad",
+                   "answer":"I love my family"
+              },
+              {
+                    "query":"What do you want to become",
+                      "answer":"I am already an engineer , I aim to become a lovable dad"
+                 }
+             ]
+    if(age == "senior citizen"):
+                    return [
+             {
+               "query": "What is a mobile" ,
+                "answer":"I am too old to use mobile, i know about telephone"
+             },
+             {
+                "query":"What happens when you get sick",
+               "answer":"I am old, so i am always sick"
+             },
+             {
+                  "query":"Do you like your mom or dad",
+                   "answer":"I dont remember whom i used to like"
+              },
+              {
+                    "query":"What do you want to become",
+                      "answer":"In next birth i want to become a social worker"
+                 }
+             ]
+def generate_response(age,task_type,user_query):
+    examples= generate_examples(age)
     sample_template="""
      Question: {query},
      Response: {answer}
@@ -58,4 +111,20 @@ def generate_response(age,task_type,user_query):
 
 
     output= llm.invoke(prompt)
-    print(output.content)
+    return output.content
+
+
+st.set_page_config(page_title="Marketing Application",
+                   page_icon= ":tick:",
+                   layout= "centered",
+                   initial_sidebar_state="collapsed")
+st.header("How can I help you today")
+form_input = st.text_area("Enter Text", height=275)
+tasktype_option = st.selectbox('Please select the action to be performed',
+                               ('Write a tweet','Write a sales copy','Write product description'),key=1)
+
+age_option = st.selectbox('Relevant to which age group',('kid','adult','senior citizen'),key=2)
+number_of_words = st.slider('Words Limit',1,200,25)
+submit = st.button('Generate')
+if submit:
+    st.write(generate_response(age_option,tasktype_option,form_input))
